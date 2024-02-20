c91234513cd71eb807dac2e532ac3e34a2cefe2e
Locu9697 <146201779+Locu9697@users.noreply.github.com>
2024-02-20 10:43:53 -0600

    Initial commit

---
+++ HelloWorld-LastName-FirstName/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld_LastName_FirstName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
